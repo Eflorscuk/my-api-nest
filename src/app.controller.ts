e8af185fefa13342951ee3bc62e210b5657acac2
Evandro Florscuk <evandro_ff@me.com>
2023-04-04 00:58:32 -0300

    DTO created

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 //import axios from 'axios';
 
@@ -17,4 +17,7 @@ export class AppController {
   async getAllUsers() {
     return this.appService.getAllUsers();
   }
+
+  @Post()
+  
 }
