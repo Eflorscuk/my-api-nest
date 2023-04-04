f067a33aa468fcf984af4780716bdbbefd82bebd
Evandro Florscuk <evandro_ff@me.com>
2023-04-04 00:40:52 -0300

    DB connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const user = 'evandroooff';
+const password = 'Florscuk2021';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${user}:${password}@apicluster.5l8vbou.mongodb.net/?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
